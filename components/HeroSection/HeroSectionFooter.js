45e0f9e362d96fd7a6e9dd9efcc1fcf124197e19
divyanshuaaliwal <divyanshuaaliwal@gmail.com>
2025-09-15 20:29:29 +0530

    Hero section updated

--- components/HeroSection/HeroSectionFooter.js
+++ components/HeroSection/HeroSectionFooter.js
@@ -4,11 +4,15 @@ import { Map } from "lucide-react";
 export default function Footer() {
     return (
         <footer className={styles.footer}>
+
             <div className={styles.location}>
                 <Map size={20} className={styles.icon} />
                 <span>Madhya Pradesh, India</span>
             </div>
+            
             <div className={styles.right}>© 2025 Sindhi Maitryuen</div>
+            
+            <div className={styles.right}>Organized by SindhuMuhinji Jijal, Indore</div>
         </footer>
     );
 }
